6da4d864ec631b8bceaa1985fc2967547007902c
messamoreira <bmf741@gmail.com>
2025-10-27 20:15:05 -0300

    feat: Implementa CRUD de contatos e autenticação de usuário

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from contact.views import contact_views, contact_forms
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('', contact_views.index, name='index'),
-    path('contact/create/', contact_forms.create, name='create'),
-    path('<int:contact_id>/', contact_views.contact, name='contact'),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # user
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]
---
+++ contact/views/user_forms.py
@@ -0,0 +1,86 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+from contact.forms import RegisterForm, RegisterUpdateForm
+
+
+def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:login')
+
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form': form,
+            'site_title': 'Register - '
+        }
+    )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+            'site_title': 'Login - '
+        }
+    )
+
+
+@login_required(login_url='contact:login')
+def logout_view(request):
+    logout(request)
+    return redirect('contact:login')
+
+
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    messages.success(request, 'Usuário atualizado com sucesso!')
+    return redirect('contact:index')
